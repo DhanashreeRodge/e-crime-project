da9eff7fa03edef566e42c9a0521b4a45cef6c84
PotdarAnjali1999 <potdaranjali1999@gmail.com>
2024-08-04 19:00:56 +0530

    BaseEntity Pojo Added

---
+++ src/main/java/com/app/entites/Complaint.java
@@ -0,0 +1,14 @@
+package com.app.entites;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="complaints")
+public class Complaint {
+	
+	
+}
